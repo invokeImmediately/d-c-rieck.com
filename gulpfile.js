02e1eec22398f244761e3b6fbe2c20c227606c19
invokeImmediately <danielcrieck@gmail.com>
2022-07-16 19:22:14 -0700

    Build: Add gulp task for compiling TS into JS
    In «gulpfile.js», add a new gulp task for compiling TS files into a JS source file to be deployed on my portfolio and sandbox website.

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@
  * Gulp automation task definition file for setting up tasks that build CSS and JS files for use on
  *   d-c-rieck.com portfolio and sandbox site.
  *
- * @version 0.0.0
+ * @version 0.1.0
  *
  * @link https://github.com/invokeImmediately/d-c-rieck.com/blob/main/gulpfile.js
  * @author Daniel Rieck [danielcrieck@gmail.com] (https://github.com/invokeImmediately)
@@ -30,7 +30,7 @@
 // TABLE OF CONTENTS
 // ----------------------------------------------------------------------------------------------
 //   §1: Persistent documentation for final output.............................................43
-//   §2: Import Node.js modules................................................................55
+//   §2: Handle Gulp task dependencies.........................................................55
 //   §3: Declare CSS build task set up.........................................................69
 //   §4: Declare JS build task set up.........................................................102
 //   §5: Declare message logging process......................................................107
@@ -52,8 +52,9 @@
 'use strict';
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-// §2: Import Node.js modules
+// §2: Handle Gulp task dependencies
 
+// --»  Import Node.js Modules  «--
 const cleanCss = require( 'gulp-clean-css' );
 const concat = require( 'gulp-concat' );
 const extName = require( 'gulp-extname' );
@@ -61,36 +62,44 @@ const gap = require( 'gulp-append-prepend' );
 const gulp = require( 'gulp' );
 const lessc = require( 'gulp-less' );
 const notify = require( 'gulp-notify' );
+const preserveTsWs = require( "gulp-preserve-typescript-whitespace" );
+const prettier = require( "gulp-prettier" );
+const pump = require( 'pump' );
 const replace = require( 'gulp-replace' );
 const terser = require( 'gulp-terser' );
-const pump = require( 'pump' );
+const ts = require( "gulp-typescript" );
+const tsProject = ts.createProject( "tsconfig.json" );
+
+// --»  Specify task names  «--
+const compCssTaskNm = "compileCss"
+const compTsTaskNm = "compileTs"
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // §3: Declare CSS build task set up
 
-function setUpCssBuildTask( ts ) {
-  gulp.task( 'buildCss', function ( callBack ) {
+function setUpCssBuildTask( tskSttngs ) {
+  gulp.task( compCssTaskNm, function ( callBack ) {
     pump( [
-      gulp.src( ts.sourceFile ).on( 'end', () => {
+      gulp.src( tskSttngs.sourceFile ).on( 'end', () => {
         logUpdate( 'Beginning CSS build process.' );
       } ),
       lessc( {
-        paths: [ts.dependenciesPath]
+        paths: [tskSttngs.dependenciesPath]
       } ).on( 'end', () => {
-        logUpdate( 'Finished compiling precompiled CSS written in the Less' +
-          ' language extension of CSS.' );
+        logUpdate( 'Finished compiling precompiled CSS written in the Less language extension of CSS.' );
       } ),
-      replace( ts.commentRemovalNeedle, '' ).on( 'end', () => {
+      replace( tskSttngs.commentRemovalNeedle, '' ).on( 'end', () => {
         logUpdate( 'Removed comments not marked as persistent.' );
       } ),
-      gulp.dest( ts.destFolder ).on( 'end', () => {
+      gulp.dest( tskSttngs.destFolder ).on( 'end', () => {
         logUpdate( 'Unminified CSS file has been built and written.' );
       } ),
       cleanCss().on( 'end', () => {
         logUpdate( 'Finished minifying CSS.' );
       } ),
-      extName( ts.minCssFileExtension ),
-      gulp.dest( ts.destFolder ).on( 'end', () => {
+      extName( tskSttngs.minCssFileExtension ),
+      gulp.dest( tskSttngs.destFolder ).on( 'end', () => {
         logUpdate( 'Minified CSS file has been built and written.' );
       } ),
       notify( 'The gulp-automated CSS build process has completed.' )
@@ -101,7 +110,35 @@ function setUpCssBuildTask( ts ) {
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // §4: Declare JS build task set up
 
-// @todo Finish writing.
+// --»  Define the task for compiling TS into JS  «--
+gulp.task( compTsTaskNm, function ( cb ) {
+  const tsResult = pump( [
+    tsProject.src(),
+    preserveTsWs.saveWhitespace().on( 'end', () => {
+      logUpdate( 'Whitespace was preserved before compiling TS into JS.' );
+    } ),
+    tsProject()
+  ], cb );
+  pump( [
+    tsResult.js,
+    preserveTsWs.restoreWhitespace().on( 'end', () => {
+      logUpdate( 'Whitespace was restored to the compiled JS.' );
+    } ),
+    prettier( { tabWidth: 2 } ).on( 'end', () => {
+      logUpdate( 'Compiled JS has been prettified.' );
+    } ),
+    gulp.dest( 'JS' ).on( 'end', () => {
+      logUpdate( 'Unminified JS file has been written.' );
+    } ),
+    terser( { output: { comments: /^!/ } } ).on( 'end', () => {
+      logUpdate( 'Finished minifying JS with Terser.' );
+    } ),
+    extName( '.min.js' ),
+    gulp.dest( 'JS' ).on( 'end', () => {
+      logUpdate( "Minified JS file has been written; '\u001b[36m" + compTsTaskNm + "\u001b[39m' now truly finished." );
+    } )
+  ], cb );
+} );
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // §5: Declare message logging process
