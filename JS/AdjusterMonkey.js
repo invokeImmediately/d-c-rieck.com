afe84a0f31497aaa03afac8202aec04e6e6793a5
invokeImmediately <danielcrieck@gmail.com>
2022-09-30 22:38:52 -0700

    Build: Update source files for AdjusterMonkey
    Rebuild «JS/AdjusterMonkey.js» and «JS/AdjusterMonkey.min.js» to incorporate development changes to their sources made in 891c75a, 992b51f, and 05eeeae.

--- JS/AdjusterMonkey.js
+++ JS/AdjusterMonkey.js
@@ -1,42 +1,4 @@
-/**=================================================================================================
- * ▄▀▀▄ █▀▀▄    █ █  █ ▄▀▀▀▐▀█▀▌█▀▀▀ █▀▀▄ ▐▀▄▀▌▄▀▀▄ ▐▀▀▄ █ ▄▀ █▀▀▀ █  █   ▐▀█▀▌▄▀▀▀
- * █▄▄█ █  █ ▄  █ █  █ ▀▀▀█  █  █▀▀  █▄▄▀ █ ▀ ▌█  █ █  ▐ █▀▄  █▀▀  ▀▄▄█     █  ▀▀▀█
- * █  ▀ ▀▀▀  ▀▄▄█  ▀▀  ▀▀▀   █  ▀▀▀▀ ▀  ▀▄█   ▀ ▀▀  ▀  ▐ ▀  ▀▄▀▀▀▀ ▄▄▄▀ ▀   █  ▀▀▀
- *
- * Userscript designed for use with the TamperMonkey web browser extension that implements a
- *   contextual, adaptable user interface for adjusting web browsing experiences.
- *
- * @version 0.0.0
- * @author Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)
- * @link https://github.com/invokeImmediately/d-c-rieck.com/blob/main/TS/TamperMonkey/AdjusterMonkey
- *   .ts
- * @license MIT Copyright (c) 2022 Daniel C. Rieck.
- *   Permission is hereby granted, free of charge, to any person obtaining a copy of this software
- *     and associated documentation files (the "Software"), to deal in the Software without
- *     restriction, including without limitation the rights to use, copy, modify, merge, publish,
- *     distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom
- *     the Software is furnished to do so, subject to the following conditions:
- *   The above copyright notice and this permission notice shall be included in all copies or
- *     substantial portions of the Software.
- *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
- *     BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- *     NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- *     DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- *     FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- * =================================================================================================
- */
-
-// Is this workable/necessary? Probably just need to update the tsconfig w/ @types/react.
-// import React from 'react';
-
-(function (
-  scriptNm,
-  scriptHotKey,
-  reactElId,
-  cdnSrc4React,
-  reactDOMElId,
-  cdnSrc4ReactDOM
-) {
+(function (iife) {
   "use strict";
 
   let adjMnkyIntf = undefined;
@@ -44,14 +6,14 @@
   //////////////////
   // § Provide a message logging interface
   function logAdjMnkyMsg(msg, ...subst) {
-    console.log(scriptNm + " —» " + msg, ...subst);
+    console.log(iife.scriptNm + " —» " + msg, ...subst);
   }
 
   //////////////////
   // § Set up triggering hotkey for activation of the Adjuster Monkey UI
 
   function chk4AdjMnkyHotKey(evt) {
-    if (evt.key == scriptHotKey && evt.ctrlKey && evt.altKey) {
+    if (evt.key == iife.scriptHotKey && evt.ctrlKey && evt.altKey) {
       logAdjMnkyMsg("My UI has been triggered.");
       toggleAdjMnkyIntf();
     }
@@ -59,13 +21,45 @@
 
   function toggleAdjMnkyIntf() {
     if (adjMnkyIntf === undefined) {
-      adjMnkyIntf = null;
+      addAdjMnkyRootEl();
       loadReactViaCDN();
     }
   }
 
   //////////////////
+  // § Add the root element for the Adjuster Monkey interface to the DOM
+
+  function addAdjMnkyRootEl() {
+    adjMnkyIntf = document.createElement("div");
+    adjMnkyIntf.id = iife.adjMnkyRootElId;
+    adjMnkyIntf.className = iife.adjMnkyBlkClass;
+    document.body.appendChild(adjMnkyIntf);
+  }
+
+  //////////////////
   // § When triggered, ensure React modules are loaded
+
+  function loadReactViaCDN() {
+    logAdjMnkyMsg("Now checking for the presence of React.");
+    if (
+      typeof React !== "undefined" ||
+      document.getElementById(iife.reactElId) !== null
+    ) {
+      reportReactPresent();
+      loadReactDOMViaCDN();
+      return;
+    }
+    addScriptToBodyTag(
+      iife.reactElId,
+      true,
+      reportReactLoadedByCDN,
+      iife.cdnSrc4React
+    );
+  }
+
+  function reportReactPresent() {
+    logAdjMnkyMsg("I see that ReactJS is already present.");
+  }
 
   function addScriptToBodyTag(elId, isCo, olCb, srcUrl) {
     const newScript = document.createElement("script");
@@ -78,23 +72,6 @@
     document.body.append(newScript);
   }
 
-  function loadReactViaCDN() {
-    logAdjMnkyMsg("Now checking for the presence of React.");
-    if (
-      typeof React !== "undefined" ||
-      document.getElementById(reactElId) !== null
-    ) {
-      reportReactPresent();
-      loadReactDOMViaCDN();
-      return;
-    }
-    addScriptToBodyTag(reactElId, true, reportReactLoadedByCDN, cdnSrc4React);
-  }
-
-  function reportReactPresent() {
-    logAdjMnkyMsg("I see that ReactJS is already present.");
-  }
-
   function reportReactLoadedByCDN() {
     logAdjMnkyMsg("React was not found, so I loaded it via CDN.");
     loadReactDOMViaCDN();
@@ -104,17 +81,18 @@
     logAdjMnkyMsg("Now checking for the presence of ReactDOM.");
     if (
       typeof ReactDOM !== "undefined" ||
-      document.getElementById(reactDOMElId) !== null
+      document.getElementById(iife.reactDOMElId) !== null
     ) {
       reportReactDOMPresent();
+      loadAdjMnkyIntfByExtHost();
       // TODO: doNextThing( … );
       return;
     }
     addScriptToBodyTag(
-      reactDOMElId,
+      iife.reactDOMElId,
       true,
       reportReactDOMLoadedByCDN,
-      cdnSrc4ReactDOM
+      iife.cdnSrc4ReactDOM
     );
   }
 
@@ -124,7 +102,25 @@
 
   function reportReactDOMLoadedByCDN() {
     logAdjMnkyMsg("ReactDOM was not found, so I loaded it via CDN.");
+    loadAdjMnkyIntfByExtHost();
     // TODO: doNextThing( … );
+  }
+
+  //////////////////
+  // § When triggered, load the AdjusterMonkey interface
+
+  function loadAdjMnkyIntfByExtHost() {
+    logAdjMnkyMsg("Now that React is present, I will load my interface.");
+    addScriptToBodyTag(
+      iife.adjMnkyJsId,
+      true,
+      reportAdjMnkyJsLoadedByExtHost,
+      iife.extHostSrc4AdjMnkyIntf
+    );
+  }
+
+  function reportAdjMnkyJsLoadedByExtHost() {
+    logAdjMnkyMsg("My interface is now loaded.");
   }
 
   //////////////////
@@ -135,22 +131,36 @@
   }
 
   window.addEventListener("load", respToPgLoad);
-})(
+})({
   // scriptNm: string ↓
-  "AdjusterMonkey.js",
+  scriptNm: "AdjusterMonkey.js",
 
   // scriptHotKey: string ↓
-  "j",
+  scriptHotKey: "j",
 
   // reactElId: string ↓
-  "react-src-code",
+  reactElId: "react-src-code",
 
   // cdnSrc4React: string ↓
-  "https://unpkg.com/react@18/umd/react.production.min.js",
+  cdnSrc4React: "https://unpkg.com/react@18/umd/react.production.min.js",
 
   // reactDOMElId: string ↓
-  "reactdom-src-code",
+  reactDOMElId: "reactdom-src-code",
 
   // cdnSrc4ReactDOM: string ↓
-  "https://unpkg.com/react-dom@18/umd/react-dom.production.min.js"
-);
+  cdnSrc4ReactDOM:
+    "https://unpkg.com/react-dom@18/umd/react-dom.production.min.js",
+
+  // adjMnkyJsId ↓
+  adjMnkyJsId: "adj-mnky-intf-src",
+
+  // extHostSrc4AdjMnkyIntf ↓
+  extHostSrc4AdjMnkyIntf:
+    "https://d-c-rieck.com/dist/adjmnky-wjb1y05v/AdjMnkyIntf.js",
+
+  // adjMnkyRootElId ↓
+  adjMnkyRootElId: "dcrdc-adjmnky-cHXlHx1Q",
+
+  // adjMnkyBlkClass ↓
+  adjMnkyBlkClass: "adjmnky",
+});
