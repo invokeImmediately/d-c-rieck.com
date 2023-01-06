invokeImmediately/d-c-rieck.com

commit e3750e842a43362efc812449433cb59340e82786

Feat: Adaptive list widening for Trello boards
In «AdjusterMonkey.Trello.js», enhance click-triggered widening of Trello lists with a mutation observer that detects and responds to the addition of new lists to the board. Also make the set up of list widening more robust by using the dataset interface to prevent multiple list widening click handlers from being set up on a list.

JS/AdjusterMonkey.Trello.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AdjusterMonkey: Trello
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Enhance Trello workflows with adjustments to CSS and JS.
 // @author       Daniel Rieck <danielcrieck@gmail.com> (https://github.com/invokeImmediately)
 // @match        https://trello.com/*
@@ -13,12 +13,12 @@
 
 /*!*************************************************************************************************
  * AdjusterMonkey.▐▀█▀▌█▀▀▄ █▀▀▀ █    █    ▄▀▀▄  · · · · · · · · · · · · · · · · · · · · · · · · · ·
- *  · · · · · · ·   █  █▄▄▀ █▀▀  █  ▄ █  ▄ █  █ · · · · · · · · · · · · · · · · · · · · · · · · · · 
+ *  · · · · · · ·   █  █▄▄▀ █▀▀  █  ▄ █  ▄ █  █ · · · · · · · · · · · · · · · · · · · · · · · · · ·
  * · · · · · · · ·  █  ▀  ▀▄▀▀▀▀ ▀▀▀  ▀▀▀   ▀▀ .js · · · · · · · · · · · · · · · · · · · · · · · · ·
  * ·································································································
  * Tampermonkey script designed to enhance Trello workflows with adjustments to CSS and JS.
  *
- * @version 0.2.1
+ * @version 0.3.0
  *
  * @author Daniel C. Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)
  * @link https://github.com/invokeImmediately/d-c-rieck.com/blob/main/JS/AdjusterMonkey.Trello.js
@@ -34,6 +34,7 @@
   console.log( 'AdjusterMonkey Script for enhancing Trello has loaded.' );
 
   function monitorTrelloLocation() {
+    console.log( 'AdjusterMonkey is now monitoring the location of the Trello web app.' );
     let currentLocation = document.location.href;
     const contentContainer = document.querySelector( '#content' );
     let timerId = null;
@@ -43,7 +44,6 @@
           return;
         }
         currentLocation = document.location.href;
-        console.log( 'Updating location to %s.', currentLocation );
         if ( timerId !== null ) {
           window.clearTimeout( timerId );
           console.log( 'Clearing timer %s.', timerId );
@@ -56,6 +56,27 @@
       childList: true,
     };
     observer.observe( contentContainer, cfg);
+  }
+
+  function monitorBoard() {
+    console.log( 'AdjusterMonkey is monitoring the board for list changes.' );
+    const board = document.querySelector( '#board' );
+    let timerId = null;
+    const observer = new MutationObserver( ( mutations ) => {
+      mutations.forEach( ( mutation ) => {
+        console.log( 'Adjuster monkey detected a change to the lists present on the board.' );
+        if ( timerId !== null ) {
+          window.clearTimeout( timerId );
+          console.log( 'Clearing timer %s.', timerId );
+          timerId = null;
+        }
+        timerId = window.setTimeout( enhanceTrello, 1000 );
+      } );
+    } );
+    const observerConfig = {
+      childList: true,
+    };
+    observer.observe( board, observerConfig );
   }
 
   function enhanceTrello() {
@@ -92,6 +113,11 @@
     console.log( 'AdjusterMonkey is setting up list widening.' );
     const cardLists = document.querySelectorAll( '.js-list' );
     cardLists.forEach( ( cardList ) => {
+      if ( cardList.dataset.hasListWidening === 'yes' ) {
+        return;
+      } else {
+        cardList.dataset.hasListWidening = 'yes';
+      }
       cardList.addEventListener( 'click', ( event ) => {
         if ( !shouldClickWidenList( cardList, event ) ) {
           return;
@@ -112,6 +138,152 @@
   window.addEventListener( 'load', ( event ) => {
     monitorTrelloLocation();
     window.setTimeout( enhanceTrello, 250 );
+    window.setTimeout( monitorBoard, 1000 );
+  } );
+} )( {
+  cardListPadding: 10,
+} );
+// ==UserScript==
+// @name         AdjusterMonkey: Trello
+// @namespace    http://tampermonkey.net/
+// @version      0.3
+// @description  Enhance Trello workflows with adjustments to CSS and JS.
+// @author       Daniel Rieck <danielcrieck@gmail.com> (https://github.com/invokeImmediately)
+// @match        https://trello.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=trello.com
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+
+
+/*!*************************************************************************************************
+ * AdjusterMonkey.▐▀█▀▌█▀▀▄ █▀▀▀ █    █    ▄▀▀▄  · · · · · · · · · · · · · · · · · · · · · · · · · ·
+ *  · · · · · · ·   █  █▄▄▀ █▀▀  █  ▄ █  ▄ █  █ · · · · · · · · · · · · · · · · · · · · · · · · · ·
+ * · · · · · · · ·  █  ▀  ▀▄▀▀▀▀ ▀▀▀  ▀▀▀   ▀▀ .js · · · · · · · · · · · · · · · · · · · · · · · · ·
+ * ·································································································
+ * Tampermonkey script designed to enhance Trello workflows with adjustments to CSS and JS.
+ *
+ * @version 0.3.0
+ *
+ * @author Daniel C. Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)
+ * @link https://github.com/invokeImmediately/d-c-rieck.com/blob/main/JS/AdjusterMonkey.Trello.js
+ * @license MIT - Copyright (c) 2022 Daniel C. Rieck
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *   THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ **************************************************************************************************/
+
+( function( iifeSettings ) {
+  'use strict';
+
+  console.log( 'AdjusterMonkey Script for enhancing Trello has loaded.' );
+
+  function monitorTrelloLocation() {
+    console.log( 'AdjusterMonkey is now monitoring the location of the Trello web app.' );
+    let currentLocation = document.location.href;
+    const contentContainer = document.querySelector( '#content' );
+    let timerId = null;
+    const observer = new MutationObserver( ( mutations ) => {
+      mutations.forEach( ( mutation ) => {
+        if ( currentLocation == document.location.href ) {
+          return;
+        }
+        currentLocation = document.location.href;
+        if ( timerId !== null ) {
+          window.clearTimeout( timerId );
+          console.log( 'Clearing timer %s.', timerId );
+          timerId = null;
+        }
+        timerId = window.setTimeout( enhanceTrello, 1000 );
+      } );
+    } );
+    const cfg = {
+      childList: true,
+    };
+    observer.observe( contentContainer, cfg);
+  }
+
+  function monitorBoard() {
+    console.log( 'AdjusterMonkey is monitoring the board for list changes.' );
+    const board = document.querySelector( '#board' );
+    let timerId = null;
+    const observer = new MutationObserver( ( mutations ) => {
+      mutations.forEach( ( mutation ) => {
+        console.log( 'Adjuster monkey detected a change to the lists present on the board.' );
+        if ( timerId !== null ) {
+          window.clearTimeout( timerId );
+          console.log( 'Clearing timer %s.', timerId );
+          timerId = null;
+        }
+        timerId = window.setTimeout( enhanceTrello, 1000 );
+      } );
+    } );
+    const observerConfig = {
+      childList: true,
+    };
+    observer.observe( board, observerConfig );
+  }
+
+  function enhanceTrello() {
+    setupListWidening();
+  }
+
+  function cycleWidenedList( cardList ) {
+    if ( cardList.classList.contains( "js-list--1xl-wide" ) ) {
+      cardList.classList.remove( "js-list--1xl-wide" )
+      cardList.classList.add( "js-list--2xl-wide" )
+    } else if ( cardList.classList.contains( "js-list--2xl-wide" ) ) {
+      cardList.classList.remove( "js-list--2xl-wide" )
+      cardList.classList.add( "js-list--3xl-wide" )
+    } else if ( cardList.classList.contains( "js-list--3xl-wide" ) ) {
+      cardList.classList.remove( "js-list--3xl-wide" )
+    }
+  }
+
+  function shouldClickWidenList( cardList, event ) {
+    const triggerLeft = iifeSettings.cardListPadding;
+    const triggerRight = cardList.clientWidth - iifeSettings.cardListPadding;
+    return event.offsetX <= triggerLeft || event.offsetX > triggerRight;
+  }
+
+  function widenNewList( cardList ) {
+    const otherCardLists = document.querySelectorAll( '.js-list' );
+    otherCardLists.forEach( ( otherList ) => {
+      otherList.classList.remove( "js-list--1xl-wide", "js-list--2xl-wide", "js-list--3xl-wide" );
+    } );
+    cardList.classList.add( "js-list--1xl-wide" );
+  }
+
+  function setupListWidening() {
+    console.log( 'AdjusterMonkey is setting up list widening.' );
+    const cardLists = document.querySelectorAll( '.js-list' );
+    cardLists.forEach( ( cardList ) => {
+      if ( cardList.dataset.hasListWidening === 'yes' ) {
+        return;
+      } else {
+        cardList.dataset.hasListWidening = 'yes';
+      }
+      cardList.addEventListener( 'click', ( event ) => {
+        if ( !shouldClickWidenList( cardList, event ) ) {
+          return;
+        }
+        if (
+          cardList.classList.contains( "js-list--1xl-wide" ) ||
+          cardList.classList.contains( "js-list--2xl-wide" ) ||
+          cardList.classList.contains( "js-list--3xl-wide" )
+        ) {
+          cycleWidenedList( cardList );
+        } else {
+          widenNewList( cardList );
+        }
+      } );
+    } );
+  }
+
+  window.addEventListener( 'load', ( event ) => {
+    monitorTrelloLocation();
+    window.setTimeout( enhanceTrello, 250 );
+    window.setTimeout( monitorBoard, 1000 );
   } );
 } )( {
   cardListPadding: 10,

commit 556d5cbe2094975eff06f3e919482e040caef1b6

Fix: Trello enhancer JS → Code duplication
Fix a code duplication error that was accidentally introduced in e3750e842a43362efc812449433cb59340e82786 to «AdjusterMonkey.Trello.js».

JS/AdjusterMonkey.Trello.js
@@ -143,148 +143,3 @@
 } )( {
   cardListPadding: 10,
 } );
-// ==UserScript==
-// @name         AdjusterMonkey: Trello
-// @namespace    http://tampermonkey.net/
-// @version      0.3
-// @description  Enhance Trello workflows with adjustments to CSS and JS.
-// @author       Daniel Rieck <danielcrieck@gmail.com> (https://github.com/invokeImmediately)
-// @match        https://trello.com/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=trello.com
-// @grant        none
-// @license      MIT
-// ==/UserScript==
-
-
-/*!*************************************************************************************************
- * AdjusterMonkey.▐▀█▀▌█▀▀▄ █▀▀▀ █    █    ▄▀▀▄  · · · · · · · · · · · · · · · · · · · · · · · · · ·
- *  · · · · · · ·   █  █▄▄▀ █▀▀  █  ▄ █  ▄ █  █ · · · · · · · · · · · · · · · · · · · · · · · · · ·
- * · · · · · · · ·  █  ▀  ▀▄▀▀▀▀ ▀▀▀  ▀▀▀   ▀▀ .js · · · · · · · · · · · · · · · · · · · · · · · · ·
- * ·································································································
- * Tampermonkey script designed to enhance Trello workflows with adjustments to CSS and JS.
- *
- * @version 0.3.0
- *
- * @author Daniel C. Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)
- * @link https://github.com/invokeImmediately/d-c-rieck.com/blob/main/JS/AdjusterMonkey.Trello.js
- * @license MIT - Copyright (c) 2022 Daniel C. Rieck
- *   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- *   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- *   THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- **************************************************************************************************/
-
-( function( iifeSettings ) {
-  'use strict';
-
-  console.log( 'AdjusterMonkey Script for enhancing Trello has loaded.' );
-
-  function monitorTrelloLocation() {
-    console.log( 'AdjusterMonkey is now monitoring the location of the Trello web app.' );
-    let currentLocation = document.location.href;
-    const contentContainer = document.querySelector( '#content' );
-    let timerId = null;
-    const observer = new MutationObserver( ( mutations ) => {
-      mutations.forEach( ( mutation ) => {
-        if ( currentLocation == document.location.href ) {
-          return;
-        }
-        currentLocation = document.location.href;
-        if ( timerId !== null ) {
-          window.clearTimeout( timerId );
-          console.log( 'Clearing timer %s.', timerId );
-          timerId = null;
-        }
-        timerId = window.setTimeout( enhanceTrello, 1000 );
-      } );
-    } );
-    const cfg = {
-      childList: true,
-    };
-    observer.observe( contentContainer, cfg);
-  }
-
-  function monitorBoard() {
-    console.log( 'AdjusterMonkey is monitoring the board for list changes.' );
-    const board = document.querySelector( '#board' );
-    let timerId = null;
-    const observer = new MutationObserver( ( mutations ) => {
-      mutations.forEach( ( mutation ) => {
-        console.log( 'Adjuster monkey detected a change to the lists present on the board.' );
-        if ( timerId !== null ) {
-          window.clearTimeout( timerId );
-          console.log( 'Clearing timer %s.', timerId );
-          timerId = null;
-        }
-        timerId = window.setTimeout( enhanceTrello, 1000 );
-      } );
-    } );
-    const observerConfig = {
-      childList: true,
-    };
-    observer.observe( board, observerConfig );
-  }
-
-  function enhanceTrello() {
-    setupListWidening();
-  }
-
-  function cycleWidenedList( cardList ) {
-    if ( cardList.classList.contains( "js-list--1xl-wide" ) ) {
-      cardList.classList.remove( "js-list--1xl-wide" )
-      cardList.classList.add( "js-list--2xl-wide" )
-    } else if ( cardList.classList.contains( "js-list--2xl-wide" ) ) {
-      cardList.classList.remove( "js-list--2xl-wide" )
-      cardList.classList.add( "js-list--3xl-wide" )
-    } else if ( cardList.classList.contains( "js-list--3xl-wide" ) ) {
-      cardList.classList.remove( "js-list--3xl-wide" )
-    }
-  }
-
-  function shouldClickWidenList( cardList, event ) {
-    const triggerLeft = iifeSettings.cardListPadding;
-    const triggerRight = cardList.clientWidth - iifeSettings.cardListPadding;
-    return event.offsetX <= triggerLeft || event.offsetX > triggerRight;
-  }
-
-  function widenNewList( cardList ) {
-    const otherCardLists = document.querySelectorAll( '.js-list' );
-    otherCardLists.forEach( ( otherList ) => {
-      otherList.classList.remove( "js-list--1xl-wide", "js-list--2xl-wide", "js-list--3xl-wide" );
-    } );
-    cardList.classList.add( "js-list--1xl-wide" );
-  }
-
-  function setupListWidening() {
-    console.log( 'AdjusterMonkey is setting up list widening.' );
-    const cardLists = document.querySelectorAll( '.js-list' );
-    cardLists.forEach( ( cardList ) => {
-      if ( cardList.dataset.hasListWidening === 'yes' ) {
-        return;
-      } else {
-        cardList.dataset.hasListWidening = 'yes';
-      }
-      cardList.addEventListener( 'click', ( event ) => {
-        if ( !shouldClickWidenList( cardList, event ) ) {
-          return;
-        }
-        if (
-          cardList.classList.contains( "js-list--1xl-wide" ) ||
-          cardList.classList.contains( "js-list--2xl-wide" ) ||
-          cardList.classList.contains( "js-list--3xl-wide" )
-        ) {
-          cycleWidenedList( cardList );
-        } else {
-          widenNewList( cardList );
-        }
-      } );
-    } );
-  }
-
-  window.addEventListener( 'load', ( event ) => {
-    monitorTrelloLocation();
-    window.setTimeout( enhanceTrello, 250 );
-    window.setTimeout( monitorBoard, 1000 );
-  } );
-} )( {
-  cardListPadding: 10,
-} );