184fea703db256f7663f50053c10d1ac89290022
invokeImmediately <danielcrieck@gmail.com>
2023-07-03 22:29:39 -0700

    Feat: Node utilities → ASCII art generator
    In the command line interface for the Node.js utility module for ASCII art generation, «Node.js/txtToAsciiArtCLI.njs», add two additional command line arguments. Make the first additional argument control whether the generated art is wrapped in a JS style comment block. Make the second additional argument control the maximum line length that constrains the process of generating ASCII art.

--- Node.js/txtToAsciiArtCLI.mjs
+++ Node.js/txtToAsciiArtCLI.mjs
@@ -1,46 +1,117 @@
+/*!*****************************************************************************
+ * ABBREVIATIONS:
+ * =============
+ * - CL(I) = Command Line (Interface)
+ ******************************************************************************/
 import { createRequire } from 'module'
 const require = createRequire(import.meta.url);
 import clipboard from 'clipboardy';
 const proc = require( 'process' );
 const txtToArtIntf = require( './txtToAsciiArt.js' );
-const valInst = "«print», «copy» (or aliases «cb» and «clipboard»), «prependToFile» (or alias «file»), and «test»."
+const validInstructions = "«print», «copy» (or aliases «cb» and «clipboard»), «prependToFile» (or alias «file»), and «test»."
 
-function printCLIHelp() {
-  console.log( `⸿ txtToAsciiArtCLI.js —» Node.js project that generates ASCII art based on a simple text string using the personal alphabet of Daniel C. Rieck.
-Command line arguments accepted by this script are as follows:
-  [ 0 ]: Simple text string to be converted.
-  [ 1 ]: An instructions for what to do with the generated art. Valid instructions are ${valInst}.` );
-}
-
-function processCLArgs( procArgs ) {
-  const opts = procArgs.slice( 2 );
-  if ( opts.length < 1 ) {
-    opts[ 1 ] = "help";
-  } else if ( opts.length < 2 ) {
-    opts[ 1 ] = "print";
-  } else if ( !opts[ 1 ].match( /^cb|clipboard|copy|file|print|test$/ ) ) {
-    throw new ReferenceError( `I do not recognize the instruction "${opts[ 1 ]}" for what to do with the ASCII art I am generating. Valid instructions are ${valInst}.` );
-  }
-  return opts;
-}
-
-function convertCLToAsciiArt( cLTxt ) {
+function convertCLToAsciiArt( cLTxt, maxLineLength, wrapInCommentBlock ) {
   const generator = new txtToArtIntf.AsciiArtGenerator();
-  return generator.getArtFromTxt( cLTxt );
+  if( wrapInCommentBlock ) {
+    maxLineLength -= 3;
+  }
+  let art = generator.getArtFromTxt( cLTxt, maxLineLength );
+  if( wrapInCommentBlock ) {
+    art = " * " + art;
+    art = art.replace( /\r\n/g, "\r\n * " );
+    art = "/**" + "*".repeat( maxLineLength ) + "\r\n" + art;
+    art += "\r\n " + "*".repeat( maxLineLength - 1 ) + "**/\r\n";
+  }
+  return art;
+}
+
+function getWrapFlagFromOpt3s( opt3s ) {
+  let wrapInCommentBlock = false;
+  if( opt3s.length >= 3 ) {
+    wrapInCommentBlock = opt3s[ 2 ];
+  }
+  return wrapInCommentBlock;
+}
+
+function getMaxLineLengthFromOpt3s( opt3s ) {
+  let maxLineLength = 80;
+  if( opt3s.length >= 4 ) {
+    maxLineLength = parseInt( opt3s[ 3 ] );
+  }
+  if( maxLineLength < 40 ) {
+    maxLineLength = 40;
+  }
+  return maxLineLength;
+}
+
+async function main() {
+  const opt3s = processCLArgs( proc.argv );
+  const maxLineLength = getMaxLineLengthFromOpt3s( opt3s );
+  const wrapInCommentBlock = getWrapFlagFromOpt3s( opt3s );
+  let art;
+  switch( opt3s[ 1 ] ) {
+  case "cb":
+  case "clipboard":
+  case "copy":
+    art = convertCLToAsciiArt( opt3s[0], maxLineLength, wrapInCommentBlock );
+    clipboard.writeSync( art );
+    break;
+  case "help":
+    printCLIHelp();
+    break;
+  case "prependToFile":
+  case "file":
+    prependToFile( opt3s[0] );
+    break;
+  case "test":
+    testPrintArtHist();
+    break;
+  case "print":
+    art = convertCLToAsciiArt( opt3s[0], maxLineLength, wrapInCommentBlock );
+    printArt( art );
+    break;
+  }
 }
 
 function printArt( art ) {
   console.log( art );
 }
 
-function testPrintArtHist() {
-  const generator = new txtToArtIntf.AsciiArtGenerator();
-  generator.getArtFromTxt( "This is a test." );
-  generator.getArtFromTxt( "Testing 1 2 3." );
-  generator.getArtFromTxt( "Testing A B C." );
-  generator.getArtFromTxt( "Audio check." );
-  generator.printArt( -1 );
-  generator.printArt( 1 );
+function printCLIHelp() {
+  console.log( `⸿ txtToAsciiArtCLI.js —» Node.js project that generates ASCII art based on a simple text string using the personal alphabet of Daniel C. Rieck.
+Command line arguments accepted by this script are as follows:
+  [ 0 ]: Simple text string to be converted.
+  [ 1 ]: An instruction for what to do with the generated art. Valid instructions are ${validInstructions}.
+  [ 2 ]: A boolean flag indicating whether the generated ASCII art should be wrapped in a JS style comment block. (Optional; default value = false)
+  [ 3 ]: Maximum line length to which the generated ASCII art should be constrained. (Optional, default value = 80)` );
+}
+
+function processCLArgs( procArgs ) {
+  const opt3s = procArgs.slice( 2 );
+
+  // First process required command line arguments.
+  if( opt3s.length < 1 ) {
+    opt3s[ 1 ] = "help";
+  } else if( opt3s.length < 2 ) {
+    opt3s[ 1 ] = "print";
+  } else if( !opt3s[ 1 ].match( /^cb|clipboard|copy|file|print|test$/ ) ) {
+    throw new ReferenceError( `I do not recognize the instruction "${opt3s[ 1 ]}" for what to do with the ASCII art I am generating. Valid instructions are ${validInstructions}.` );
+  }
+
+  // Next, begin processing optional command line arguments starting with the flag that indicates whether the art should be wrapped in a comment block.
+  if( opt3s.length >= 3 && !opt3s[ 2 ].match( /^false|true$/ ) ) {
+    throw new ReferenceError( `I do not recognize the flag "${opt3s[ 2 ]}" for indicating whether the generated art should be wrapped in a JavaScript style comment block. A valid flag is either «true» or «false».` );
+  } else if( opt3s.length >= 3 ) {
+    opt3s[ 2 ] = opt3s[ 2 ] === 'true';
+  }
+  if( opt3s.length >= 4 && !opt3s[ 3 ].match( /^[0-9]+$/ ) ) {
+    throw new ReferenceError( `I do not recognize the maximum line length value of "${opt3s[ 3 ]}" for constraining generation of the ASCII art. Only an integer value will be accepted.` );
+  }
+
+  // TODO: Test additional options
+  // TODO: Handle case of what happens with different option combinations
+
+  return opt3s;
 }
 
 function prependToFile( path, maxLineLen = 80 ) {
@@ -57,31 +128,14 @@ function prependToFile( path, maxLineLen = 80 ) {
   // - If a header comment is present, insert the ASCII art at the top of the file header comment block. <··
 }
 
-async function main() {
-  const opts = processCLArgs( proc.argv );
-  let art;
-  switch( opts[ 1 ] ) {
-  case "cb":
-  case "clipboard":
-  case "copy":
-    art = convertCLToAsciiArt( opts[0] );
-    clipboard.writeSync( art );
-    break;
-  case "help":
-    printCLIHelp();
-    break;
-  case "prependToFile":
-  case "file":
-    prependToFile( opts[0] );
-    break;
-  case "test":
-    testPrintArtHist();
-    break;
-  case "print":
-    art = convertCLToAsciiArt( opts[0] );
-    printArt( art );
-    break;
-  }
+function testPrintArtHist() {
+  const generator = new txtToArtIntf.AsciiArtGenerator();
+  generator.getArtFromTxt( "This is a test." );
+  generator.getArtFromTxt( "Testing 1 2 3." );
+  generator.getArtFromTxt( "Testing A B C." );
+  generator.getArtFromTxt( "Audio check." );
+  generator.printArt( -1 );
+  generator.printArt( 1 );
 }
 
 main();
