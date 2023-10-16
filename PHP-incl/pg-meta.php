e48c2a7aee674ad2e66975f713b85dfbdf57f384
invokeImmediately <danielcrieck@gmail.com>
2023-10-15 22:30:49 -0700

    Refactor: PHP Incl. → Page metadata and links
    Clean up code in «PHP-incl\pg-meta.php» as follows:
    - Touch up the file header comment's format.
    - Rename arguments of and variables in method «DcrdcPgMeta.
       OutputFavIconLines» for easier reading.
    - Fix a mistake in a conditional within «DcrdcPgMeta.
       OutputFavIconLines».
    - Check and update inline documentation.

--- PHP-incl/pg-meta.php
+++ PHP-incl/pg-meta.php
@@ -1,72 +1,78 @@
 <?php
+
 /**
- * ▄▀▀▀ ▀█▀▐▀█▀▌█▀▀▀    ▄▀▀▀ ▄▀▀▄ ▐▀▀▄ █▀▀▀ ▀█▀ █▀▀▀   █▀▀▄ █  █ █▀▀▄
- * ▀▀▀█  █   █  █▀▀  ▀▀ █    █  █ █  ▐ █▀▀▀  █  █ ▀▄   █▄▄▀ █▀▀█ █▄▄▀
- * ▀▀▀  ▀▀▀  █  ▀▀▀▀     ▀▀▀  ▀▀  ▀  ▐ ▀    ▀▀▀ ▀▀▀▀ ▀ █    █  ▀ █
+ * ▓▓▓ PHP Includes: ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+ * ▓▓▒ ─────────────────────────────── ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+ * ▓▒▒ █▀▀▄ █▀▀▀    ▐▀▄▀▌█▀▀▀▐▀█▀▌▄▀▀▄ ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓
+ * ▒▒▒ █▄▄▀ █ ▀▄ ▀▀ █ ▀ ▌█▀▀   █  █▄▄█ ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓
+ * ▒▒▒ █    ▀▀▀▀    █   ▀▀▀▀▀  █  █  ▀ .php ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓
  *
  * Provides page configuration settings that are used on all pages.
  *
- * @version 0.1.0
+ * @version 0.1.1
  *
- * @author Daniel C. Rieck [danielcrieck@gmail.com] (https://github.com/invokeImmediately)
+ * @author Daniel C. Rieck
+ *  [danielcrieck@gmail.com]
+ *  (https://github.com/invokeImmediately)
+ *
  * @link https://github.com/invokeImmediately/d-c-rieck.com/blob/main/PHP-incl/pg-meta.php
- * @license MIT Copyright (c) 2021 Daniel C. Rieck
- *   Permission is hereby granted, free of charge, to any person obtaining a copy of this software
- *     and associated documentation files (the "Software"), to deal in the Software without
- *     restriction, including without limitation the rights to use, copy, modify, merge, publish,
- *     distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom
- *     the Software is furnished to do so, subject to the following conditions:
- *   The above copyright notice and this permission notice shall be included in all copies or
- *     substantial portions of the Software.
- *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
- *     BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- *     NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- *     CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- *     ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *     SOFTWARE.
+ * @license MIT Copyright (c) 2023 Daniel C. Rieck
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *   copy of this software and associated documentation files
+ *   (the "Software"), to deal in the Software without restriction, including
+ *   without limitation the rights to use, copy, modify, merge, publish,
+ *   distribute, sublicense, and/or sell copies of the Software, and to permit
+ *   persons to whom the Software is furnished to do so, subject to the
+ *   following conditions:
+ *  The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ *   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *   DEALINGS IN THE SOFTWARE.
  */
 
-// Before pulling in code from source files, ensure we have a root path to work with.
-if( !function_exists( 'defineRootPath' ) ) {
-  function defineRootPath() {
-    if ( !defined( 'ROOT_PATH' ) ) {
-      define( 'ROOT_PATH', $_SERVER['DOCUMENT_ROOT'] . 'dcrdc' );
-    }
-  }
-}
-defineRootPath();
+// ·> Before pulling in code from source files, ensure we have a root path to  ·
+// ·  work with.                                                              <·
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/php-incl/defineSitePaths.php' );
 
-// Pull in the dependency for making database connections.
+// ·> Pull in the dependencies for making database connections.               <·
 require_once( ROOT_PATH . '/php-incl/db-conn.php' );
 require_once( ROOT_PATH . '/php-incl/res-id-loc.php' );
 
-// Declare the DcrdcPgMeta class.
+// ·> Declare the DcrdcPgMeta class.                                          <·
 class DcrdcPgMeta {
+
+  // ·> Protected properties §                                                <·
   protected $dbConn;
   protected $pgId;
   protected $rowLimits = array( "favIcos"=>64 );
 
+  // ·> Constructor §                                                         <·
   public function __construct( $dbConn, $pgId ) {
-    // Ensure a valid connection to the site database.
+    // ·> Ensure a valid connection to the site database.                     <·
     if ( empty( $dbConn ) || !( $dbConn instanceof DcrdcDbConn ) ) {
       $this->RepFatalErr( "I was not supplied with a valid connection to the site database from which to obtain page metadata." );
     } else {
       $this->dbConn = $dbConn;
     }
-    // Finish writing.
+    // ·> TO-DO: Finish writing constructor.                                  <·
   }
 
+  // ·> Public Methods §                                                      <·
   public function GetPgId() {
     return $this->pgId;
   }
 
-  public function FavIcoLnsOut( $indtnLvl, $indtn1stLn ) {
-    $indtn = $this->GetIndtn( $indtnLvl );
-    $favIcons = $this->GetSiteFavIcos();
+  public function OutputFavIconLines( $ind7nLevel, $firstLineInd7n = false ) {
+    $ind7n = $this->GetInd7n( $ind7nLevel ); // TO-DO: Write this function.
+    $favIcons = $this->GetSiteFavIcons();
     $loopLimit = $this->$rowLimits[ "favIcos" ];
     $i = 0;
     $lineCount = 0;
-    $indtn = $this->GetIndtn( $indtnLvl );
     while ( $row = $favIcos->fetch_assoc() && $i < $loopLimit ) {
       // Proceed only if the favicon is still active.
       $isActive = $row[ "active" ];
@@ -75,7 +81,7 @@ class DcrdcPgMeta {
         continue;
       }
 
-      // Get the URL to the favicon image file.
+      // ·> Get the URL to the favicon image file.                            <·
       $uriId = $row[ "uri_id" ];
       $uri = $this->GetUriFromId( $uriId );
       $type = $row[ "type" ];
@@ -83,28 +89,28 @@ class DcrdcPgMeta {
       $sizes = $row[ "sizes" ];
       $color = $row[ "hex_color" ];
 
-      // Output indententation.
-      if ( $i == 0 && $indtn1stLn == true ) {
-        echo $indtn;
+      // ·> Output indentation.                                               <·
+      if ( $i > 0 || $firstLineInd7n == true ) {
+        echo $ind7n;
       }
 
-      // Output link starting with rel and type attributes.
+      // ·> Output link starting with rel and type attributes.                <·
       echo "<link rel=\"$rel\" type=\"$type\"";
 
-      // Output the sizes attribute, if appropriate.
+      // ·> Output the sizes attribute, if appropriate.                       <·
       if ( $sizes ) {
         echo " sizes=\"$sizes\"";
       }
 
-      // Output the color attribute, if appropriate.
+      // ·> Output the color attribute, if appropriate.                       <·
       if ( $color ) {
         echo " color=\"$color\"";
       }
 
-      // Output the href attribute and complete the tag.
+      // ·> Output the href attribute and complete the tag.                   <·
       echo "href=\"$uri\">\n";
 
-      // Increment the loop's iteration tracking variables.
+      // ·> Increment the loop's iteration tracking variables.                <·
       $lineCount++;
       $i++;
     }
@@ -113,8 +119,12 @@ class DcrdcPgMeta {
     }
   }
 
-  protected function GetSiteFavIcos() {
-    $qStr = "SELECT `uri_id` FROM `site_config` WHERE `setting_type`='favico' AND `active`=1";
+  // ·> Protected methods §                                                   <·
+  protected function GetSiteFavIcons() {
+    // ·> TO-DO: Rewrite this function. Use an inner join as started below.    ·
+    // ·   $qStr = "SELECT `uri_id` FROM `site_config` INNER JOIN `…` … WHERE  ·
+    // ·     `setting_type`='favico' AND `active`=1  ";                       <·
+    $qStr = "SELECT `uri_id` FROM `site_config` WHERE `setting_type`='favico' AND `active`=1";    
     $favIcos = $this->dbConn->RunQuery( $this->qStr );
     return $favIcos;
   }
@@ -127,7 +137,7 @@ class DcrdcPgMeta {
     die( $finalMsg );
   }
 
-  // Finish writing class.
+  // ·> TO-DO: Finish writing class.                                          <·
 }
 
 ?>
